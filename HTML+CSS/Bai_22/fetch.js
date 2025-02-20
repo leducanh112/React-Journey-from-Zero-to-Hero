93cd59499845a46ab82617beb256d55b2b6eb4b8
leducanh112 <duca1122001@gmail.com>
2025-02-21 02:20:20 +0700

    Bai_22

--- HTML+CSS/Bai_22/fetch.js
+++ HTML+CSS/Bai_22/fetch.js
@@ -1,43 +1,27 @@
-//GET API
-const fetchApi = (url) => {
-  const result = fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-      return data;
-    });
-  return result;
-};
-fetchApi("https://dummyjson.com/products/categories").then((data) =>
-  console.log(data)
-);
-
+import { fetchApi } from "./helpers/fetchApi.js";
 //Get category
-fetch("https://dummyjson.com/products/categories")
-  .then((response) => response.json())
-  .then((data) => {
-    let htmls = "";
-    data.forEach((item) => {
-      htmls += `
+fetchApi("http://localhost:3000/categories").then((data) => {
+  let htmls = "";
+  data.forEach((item) => {
+    htmls += `
                 <div class="category-item">${item.slug}</div>
             `;
-    });
-    const divCategory = document.querySelector("#category");
-    divCategory.innerHTML += htmls;
   });
+  const divCategory = document.querySelector("#category");
+  divCategory.innerHTML += htmls;
+});
 
-fetch("https://dummyjson.com/products")
-  .then((response) => response.json())
-  .then((data) => {
-    let htmls = "";
-    data.products.forEach((item) => {
-      htmls += `
-            <div class="product-item">
-                    <img src="${item.thumbnail}" alt="tiêu đề ..." />
-                    <h3>${item.title}</h3>
-                    <p>${item.price}</p>
-                </div>
-            `;
-    });
-    const productItem = document.querySelector("#products");
-    productItem.innerHTML += htmls;
+fetchApi("http://localhost:3000/products").then((data) => {
+  let htmls = "";
+  data.products.forEach((item) => {
+    htmls += `
+          <div class="product-item">
+                  <img src="${item.thumbnail}" alt="tiêu đề ..." />
+                  <h3>${item.title}</h3>
+                  <p>${item.price}</p>
+              </div>
+          `;
   });
+  const productItem = document.querySelector("#products");
+  productItem.innerHTML += htmls;
+});

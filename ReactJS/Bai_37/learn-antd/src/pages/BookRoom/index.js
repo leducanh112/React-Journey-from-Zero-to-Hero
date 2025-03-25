b33ac93d5093196ea8bbb47fe256192b3fbe2ba2
leducanh112 <duca1122001@gmail.com>
2025-03-26 04:12:45 +0700

    bai 38

--- ReactJS/Bai_37/learn-antd/src/pages/BookRoom/index.js
+++ ReactJS/Bai_37/learn-antd/src/pages/BookRoom/index.js
@@ -1,5 +1,12 @@
+import { Input, Row, Col } from "antd";
 function BookRoom() {
-  return <>BookRoom</>;
+  return (
+    <>
+      <h2>Book Room</h2>
+
+      <form></form>
+    </>
+  );
 }
 
 export default BookRoom;
